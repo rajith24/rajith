2a903bec7a178c60d90466a192e7ec207939a431
rajith24 <rajithrahul91@gmail.com>
2023-10-04 20:05:58 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv,searchForWorkspaceRoot  } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
@@ -14,7 +14,9 @@ export default defineConfig(({ mode }) => {
       // Specify the custom server entry point
       fs: {
         // Make sure to use the correct path to your server file
-        allow: [path.resolve(__dirname, 'server/server.cjs')],
+        allow: [
+          searchForWorkspaceRoot(process.cwd()),
+          path.resolve(__dirname, 'server.cjs')],
       },
     },
     // server: 'server/server.js',
@@ -37,11 +39,11 @@ export default defineConfig(({ mode }) => {
         // Add the custom server entry point to the input options
         input: {
           main: path.resolve(__dirname, 'src/main.jsx'), // Replace with your main entry file
-          server: path.resolve(__dirname, 'server/server.cjs'), // Add your custom server entry file here
+          server: path.resolve(__dirname, 'server.cjs'), // Add your custom server entry file here
         },
       },
       outDir: 'dist',
-      sourcemap: false,
+      sourcemap: true,
       minify: true,
       chunkSizeWarningLimit: 10000000, // Set your preferred limit in bytes
     }
