8d9183a36165c0c0d6b0eb642cf54d5d0b2ed999
rajith24 <rajithrahul91@gmail.com>
2023-10-04 16:58:34 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:"/rajith/",
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 10000000, // Set your preferred limit in bytes
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+
+    base: "/rajith/",
+    plugins: [react()],
+    server: 'server/server.js',
+    // server: {
+
+    //   proxy: {
+    //     "^/api": {
+    //       target: env.VITE_PORT,
+    //       changeOrigin: true,
+    //       secure: false,
+    //       withCredentials: true,
+    //       rewrite: (path) => path.replace(/^\/api/, ``),
+    //     },
+    //     port: env.VITE_PORT,
+    //   },
+    // },
+    build: {
+      outDir: 'dist',
+      assetsDir: '',
+      sourcemap: false,
+      minify: true,
+      chunkSizeWarningLimit: 10000000, // Set your preferred limit in bytes
+    }
+  }
 })
